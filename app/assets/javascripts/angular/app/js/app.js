d3fb18edff7da852d50ab00c76a72034d9a6b306
Steven Barragan Naranjo <me@steven.mx>
2013-07-17 19:00:17 -0500

    [WIP] Tring to save a new team

--- app/assets/javascripts/angular/app/js/app.js
+++ app/assets/javascripts/angular/app/js/app.js
@@ -4,6 +4,7 @@
 angular.module('tacosApp', ['tacosApp.filters', 'tacosApp.services', 'tacosApp.directives', 'tacosApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/users', {templateUrl: parcial_path + 'users.html', controller: 'UserCtrl'});
+    $routeProvider.when('/users/:action', {templateUrl: parcial_path + 'users.html', controller: 'UserCtrl'});
     $routeProvider.when('/teams', {templateUrl: parcial_path + 'teams.html', controller: 'TeamCtrl'});
-    $routeProvider.otherwise({redirectTo: '/teams'});
+    $routeProvider.otherwise({redirectTo: '/users'});
   }]);
--- app/assets/javascripts/angular/app/js/controllers.js
+++ app/assets/javascripts/angular/app/js/controllers.js
@@ -2,9 +2,19 @@
 
 /* Controllers */
 angular.module('tacosApp.controllers', []).
-  controller('UserCtrl', ['$scope' , 'Users', function($scope , Users) {
-    $scope.users = Users.query()
-  }]).
+  controller('UserCtrl', ['$scope', '$routeParams', 'Users', function($scope, $routeParams, Users) {
+  $scope.users = Users.query()
+  $scope.submit = function(){
+  }
+}]).
   controller('TeamCtrl', ['$scope' , 'Teams', function($scope , Teams) {
-    $scope.teams = Teams.query()
-  }])
+  $scope.teams = Teams.query()
+  $scope.submit = function(){
+    team = Teams.get()
+    team.name = $scope.team.name
+    team.save()
+    // console.log(Teams.get())
+    // console.log( $scope.team.name )
+    // console.log( $scope.team)
+  }
+}])
